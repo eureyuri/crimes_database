b57d243596bb006d5d57a9fe1fec981313046b8a
eureyuri <eureynoguchi@gmail.com>
2020-11-16 22:23:38 -0500

    Script for generating csv to be inserted into db

---
+++ clean_data.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+import datetime
+
+df = pd.read_csv('SHR76_17.csv', sep=',', nrows=10000, skiprows=range(1, 750000))
+# df = pd.read_csv('SHR76_17.csv', sep=',', nrows=10)
+
+# Crimes
+df = df[['ID', 'Year', 'Month']]
+df = df.rename({'ID': 'case_id', 'Year': 'year', 'Month': 'month'}, axis=1)
+for index, row in df.iterrows():
+    month = row['month']
+    month_num = datetime.datetime.strptime(month, '%B').month
+    df.loc[index, 'month'] = month_num
+
+df.to_csv('crimes.csv', sep=',', index=False)
